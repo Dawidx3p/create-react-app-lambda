2d8a4b83cfddbc4d36858ab317e0732273a0782d
Dawidx3p <dawiddebiecx@gmail.com>
2020-11-14 01:22:57 +0100

    Setting up react-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import './App.css';
+import img from './profile.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
     export default function App() {
       const [status, setStatus] = useState(true);
@@ -16,24 +21,34 @@ import './App.css';
         };
         return (
           <div className="App">
-            <div id="contactBlock">
-              <h1>Contact Me via Email</h1>
-              <p>> dawiddebiecx@gmail.com</p>
+
+            <nav>
+              <h1>Dawid Dębiec</h1>
+              <ul class="navigation-items">
+                <li>About me</li>
+                <li>Projects</li>
+                <li onClick={slide}>Contact</li>
+              </ul>
+              <div id="contactBlock">
+              <h1>Contact me via E-mail</h1>
+              <p>dawiddebiecx@gmail.com</p>
             </div>
-            <section className="text">
-              <h1>About Me</h1>
-              <p>Hi, I'm creative soul that happen to be attracted to programming for some reason. Currently learning on Codecademy, great place to practice programming. 
-                Nice to meet You, If you are interested in my work You can see my projects below
-              </p>
-            <h1>My projects</h1>
-            <li>first one</li>
-            </section>
-            <section className="img">
-              <img src="profile.jpg"/>
-            </section>
-            <section className="contact">
-              <button className="contact-button" onClick={slide}>Contact</button>
-            </section>
+            </nav>
+            <Container>
+              <Row>
+              <Col xs={12} md={6}>
+                <h1>About Me</h1>
+                <p>Hi, I'm creative soul that happen to be attracted to programming for some reason. Currently learning on Codecademy, great place to practice programming. 
+                  Nice to meet You, If you are interested in my work You can see my projects below
+                </p>
+                <h1>My projects</h1>
+                <li>first one</li>
+              </Col>
+              <Col xs={12} md={6}>
+                <img src={img} alt="Profile"/>
+              </Col>
+              </Row>
+            </Container>
           </div>
         )
     }
